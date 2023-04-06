93f755084b2644cb262c8d03ddbee2857512ca46
Tzehong112002 <tzehong112002@gmail.com>
2023-04-06 22:36:18 +0800

    version event user side

--- AssFundraisingSystem/UserSide/program.aspx.designer.cs
+++ AssFundraisingSystem/UserSide/program.aspx.designer.cs
@@ -15,21 +15,12 @@ namespace AssFundraisingSystem.UserSide
     {
 
         /// <summary>
-        /// btnView control.
+        /// RepeaterEventList control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnView;
-
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Repeater RepeaterEventList;
     }
 }
