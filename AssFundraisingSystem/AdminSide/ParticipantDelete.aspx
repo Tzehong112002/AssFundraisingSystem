4dc8465f1af6bb5d641f8af9bed19bc128938c3b
danny312-dot <129974537+danny312-dot@users.noreply.github.com>
2023-04-09 15:21:45 +0800

    Update paticipant delete

--- AssFundraisingSystem/AdminSide/ParticipantDelete.aspx
+++ AssFundraisingSystem/AdminSide/ParticipantDelete.aspx
@@ -1,3 +1,25 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminSide/adminMaster.Master" AutoEventWireup="true" CodeBehind="ParticipantDelete.aspx.cs" Inherits="AssFundraisingSystem.AdminSide.ParticipantDelete" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <link href="css/BUpdate.css" rel="stylesheet" />
+<div class="title"><h1>Delete</h1> </div>
+    <p> Are you sure you wan to delete the following record?</p>
+     <table>
+                <tr>
+                    <td>Id: </td>
+                    <td>
+                        <asp:Label ID="lblId" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+
+                 <tr>
+                    <td>Name: </td>
+                    <td>
+                        <asp:Label ID="lblName" runat="server" Text="" ></asp:Label>
+                    </td>
+                </tr>      
+            </table>
+                <br />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+            <asp:Button ID="btnCancel" runat="server" Text="Cancel" OnClick="btnCancel_Click" />
+
 </asp:Content>
--- AssFundraisingSystem/AdminSide/ParticipantDelete.aspx.cs
+++ AssFundraisingSystem/AdminSide/ParticipantDelete.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,76 @@ namespace AssFundraisingSystem.AdminSide
 {
     public partial class ParticipantDelete : System.Web.UI.Page
     {
+        string cs = ConfigurationManager.ConnectionStrings["MYConnectionString"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            if (!Page.IsPostBack)
+            {//                                       if
+                string id = Request.QueryString["id"] ?? "";
+                bool found = false;
+
+                //
+                string sql = "SELECT * FROM Account WHERE UserID = @Id";
+
+                //
+                SqlConnection con = new SqlConnection(cs);
+
+                //
+                SqlCommand cmd = new SqlCommand(sql, con);
+                //
+                cmd.Parameters.AddWithValue("@Id", id);
+                //
+                con.Open();
+                //
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    //record found
+                    found = true;
+                    lblId.Text = dr[0].ToString();
+                    lblName.Text = (string)dr["Name"];
+
+                }
+                //
+                dr.Close();
+                con.Close();
+
+                if (!found)
+                {
+                    Response.Redirect("ParticipantsRecord.aspx");
+                }
+
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("ParticipantsRecord.aspx");
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            //retrive id from url
+            string id = Request.QueryString["id"];
+            //
+            string sql = "DELETE FROM Account WHERE UserID = @Id";
+
+            SqlConnection con = new SqlConnection(cs);
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            Response.Write("<script>alert('Successful')</script>");
+            Response.Redirect("ParticipantsRecord.aspx");
         }
     }
 }
